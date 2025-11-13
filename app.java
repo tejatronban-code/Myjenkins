923aca575dd98102513a7a47f6f4a6a636da9bd0
teja <tejatronban@gmail.com>
2025-11-13 14:31:52 +0530

    Add comments for clarity in app.java
    Added comments to explain the purpose of the code.

--- app.java
+++ app.java
@@ -20,3 +20,6 @@ public class MultiplyTwoNumbers {
 }
 new code by dev-1 without pull the code
 we are adding some more data to learn about Pull
+
+This is my first edit for my java application
+    
